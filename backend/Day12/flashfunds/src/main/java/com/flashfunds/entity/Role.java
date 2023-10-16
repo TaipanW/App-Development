910138155ec0acde78370b3ab99f42be7c6272b4
Arjun K Shibu <84029508+arjunkshibu@users.noreply.github.com>
2023-10-16 10:28:59 +0530

    Delete backend/Day12/flashfunds directory

--- backend/Day12/flashfunds/src/main/java/com/flashfunds/entity/Role.java
+++
@@ -1,8 +0,0 @@
-package com.flashfunds.entity;
-import lombok.RequiredArgsConstructor;
-
-@RequiredArgsConstructor
-public enum Role {
-    ADMIN,
-    USER
-}
--- backend/Day12/flashfunds/src/main/java/com/flashfunds/repository/LoanRequestRepository.java
+++
@@ -1,9 +0,0 @@
-package com.flashfunds.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.flashfunds.entity.LoanRequestEntity;
-
-public interface LoanRequestRepository extends JpaRepository<LoanRequestEntity, Long> {
-
-}
